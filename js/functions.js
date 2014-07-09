deb9e5b1a51bd64547ac699519b40b3c22576cb6
Gian Johansen <gian.johansen@gmail.com>
2014-07-09 23:34:31 +1000

    Restyled site

--- js/functions.js
+++ js/functions.js
@@ -1,18 +1,13 @@
+/* "Scroll to" links */
 $("#nav-arrow-skills").click(function() {
     $('html, body').stop(true, true).animate({
-        scrollTop: $("#skills").offset().top - 12
-    }, 2000);
-    return false;
-});
-$("#nav-arrow-portfolio").click(function() {
-    $('html, body').stop(true, true).animate({
-        scrollTop: $("#portfolio").offset().top - 12
+        scrollTop: $("#skills").offset().top
     }, 2000);
     return false;
 });
 $("#nav-arrow-contact, #navbar-contact").click(function() {
     $('html, body').stop(true, true).animate({
-        scrollTop: $("#contact").offset().top - 12
+        scrollTop: $("#contact-header").offset().top
     }, 2000);
     return false;
 });
@@ -21,22 +16,4 @@ $("#navbar-brand-wrapper").click(function() {
         scrollTop: 0
     }, 2000);
     return false;
-});
-
-/* Contact form submission */
-$( document ).ready(function() {
-  $('#contact-form').submit(function(form) {
-    // validate form
-    if ($("#contact-form").valid()) {
-      $.ajax({
-        dataType: 'jsonp',
-        url: "http://getsimpleform.com/messages/ajax?form_api_token=eca2adb05b1ade6f67db49e9bdab0822",
-        data: $('#contact-form').serialize() 
-      }).done(function() {
-        $('#contact-form button').css("background-color", "#27ae60");
-        $('#contact-form button').html("<i class='fa fa-check'></i>");
-      });
-      return false;
-    }
-  });
 });
