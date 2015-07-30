94005a34d32aa58cafe24782befbc85fb2d83204
Gian Johansen <gian.johansen@gmail.com>
2015-07-30 22:50:07 +1000

    Updated to fullscreen video

---
+++ assets/functions.js
@@ -0,0 +1,38 @@
+// cross-browser fade
+function fadeIn( elem, ms )
+{
+  if( ! elem )
+    return;
+
+  elem.style.opacity = 0;
+  elem.style.filter = "alpha(opacity=0)";
+  elem.style.display = "block";
+  elem.style.visibility = "visible";
+
+  if( ms )
+  {
+    var opacity = 0;
+    var timer = setInterval( function() {
+      opacity += 50 / ms;
+      if( opacity >= 1 )
+      {
+        clearInterval(timer);
+        opacity = 1;
+      }
+      elem.style.opacity = opacity;
+      elem.style.filter = "alpha(opacity=" + opacity * 100 + ")";
+    }, 50 );
+  }
+  else
+  {
+    elem.style.opacity = 1;
+    elem.style.filter = "alpha(opacity=1)";
+  }
+}
+
+// give video a second of buffer time
+setTimeout(function() { 
+	document.getElementById("preload").style.display = "none";
+	document.body.style.background = "#eee";
+	fadeIn(document.getElementById("hero"), 300);
+}, 1000);
