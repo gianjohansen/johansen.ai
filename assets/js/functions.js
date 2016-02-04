c79454199c474e615018c3b706cb3407d2a94227
Gian Johansen <gian.johansen@gmail.com>
2016-02-05 02:02:11 +1100

    Add panning/zooming

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -61,9 +61,29 @@ $(document).ready(function() {
     $("#portfolio .vertical").addClass("rollOut");
     $('#portfolio .vertical').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
       $("#portfolio").hide();
-      $("#playground").fadeIn();
+      $("#playground").fadeIn(function() {
+      });
+      var $panzoom = $('.panzoom').panzoom({
+        startTransform: 'scale(1)',
+        increment: 0.1,
+        minScale: 1,
+        maxScale: 2,
+        contain: 'invert',
+        cursor: 'url(assets/img/mouse-pan.png), auto'
+      });
+      $panzoom.parent().on('mousewheel.focal', function( e ) {
+        e.preventDefault();
+        var delta = e.delta || e.originalEvent.wheelDelta;
+        var zoomOut = delta ? delta < 0 : e.originalEvent.deltaY > 0;
+        e.clientY += 500;
+        $panzoom.panzoom('zoom', zoomOut, {
+          increment: 0.1,
+          animate: true,
+          focal: e
+        });
+        console.log(e);
+      });
     });
-
     return false;
   });
 });
