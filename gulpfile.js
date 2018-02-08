b17da71c59f32fdbd66381d1a6606db7d67b24a1
Gian Johansen <gian.johansen@gmail.com>
2018-02-08 14:40:04 +1100

    Add autoprefixes to SCSS

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,15 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
 
 // compile scss
 gulp.task('sass', function(){
   return gulp.src('assets/css/**/*.scss')
     .pipe(sass()) 
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
     .pipe(gulp.dest('assets/css'))
 });
 
