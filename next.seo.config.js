844eb00f3d9eb6a2e1a7072e33ab112e91c10ae1
Gian Johansen <gian.johansen@gmail.com>
2022-09-29 22:59:54 +1000

    Add default image meta tag

--- next.seo.config.js
+++ next.seo.config.js
@@ -4,6 +4,15 @@ export default {
     locale: 'en_US',
     url: 'https://johansen.software/',
     site_name: 'Gian Johansen • Front-End Engineer',
+    images: [
+      {
+        url: 'https://johansen.software/images/johansen-software-card.jpg',
+        width: 1200,
+        height: 628,
+        alt: 'Gian Johansen • Front-End Engineer',
+        type: 'image/jpeg',
+      },
+    ],
   },
   twitter: {
     site: '@gianjohansen',
